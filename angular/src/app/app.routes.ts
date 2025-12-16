9fc10c766f4d90f212fb5526b1ea85636201ad8e
Matt Beres <mattyberes@gmail.com>
2025-12-15 19:34:43 -0500

    favicon and titles

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -12,21 +12,21 @@ export const routes: Routes = [
         path: '', redirectTo: 'home', pathMatch: 'full'
     },
     {
-        path: 'home', component: Home,
+        path: 'home', component: Home, title: 'Kyber Strategy Group - Home',
     },
     {
-        path: "testimonials", component: Testimonials
+        path: "testimonials", component: Testimonials, title: 'Kyber Strategy Group - Testimonials'
     },
     {
-        path: 'contact-us', component: ContactUs
+        path: 'contact-us', component: ContactUs, title: 'Kyber Strategy Group - Contact Us'
     },
     {
-        path: 'privacy', component: Privacy
+        path: 'privacy', component: Privacy, title: 'Kyber Strategy Group - Privacy Policy'
     },
     {
-        path: 'terms', component: Terms
+        path: 'terms', component: Terms, title: 'Kyber Strategy Group - Terms of Service'
     },
     {
-        path: 'book-consultation', component: Book
+        path: 'book-consultation', component: Book, title: 'Kyber Strategy Group - Book a Consultation'
     }
 ];
